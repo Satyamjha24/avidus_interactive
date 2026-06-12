3080b67d9bc9de6da18cd59e72d64d5967970355
jhasatyam18 <satyam.jha@datamotive.io>
2026-06-12 15:53:18 +0530

    Testing api

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+console.log("API BASE URL:", import.meta.env.VITE_API_URL);
 
 axiosInstance.interceptors.request.use(
   (config) => {
